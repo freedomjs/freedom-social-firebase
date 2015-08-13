9b415fe57e596a9c8d6b5d51a4eb2ef34e0733a1
Daniel Borkan <dborkan@dborkan-macbookpro2.roam.corp.google.com>
2015-08-13 15:43:46 -0400

    Invite mode for Google+

--- src/facebook-social-provider.js
+++ src/facebook-social-provider.js
@@ -6,17 +6,37 @@ FacebookSocialProvider = function(dispatchEvent) {
 };
 FacebookSocialProvider.prototype = new FirebaseSocialProvider();
 
-FacebookSocialProvider.prototype.authenticate_ = function(firebaseRef) {
-  return this.oauth_(firebaseRef);
-};
-
 /*
  * Returns a Promise which fulfills with an OAuth token.
  */
-FacebookSocialProvider.prototype.getOAuthToken_ = function() {
+FacebookSocialProvider.prototype.getOAuthToken_ = function(loginOpts) {
+  if (loginOpts.interactive) {
+    return this.getOAuthTokenInteractive_(loginOpts);
+  }
+
+  // For non-interactive logins, attempt to re-use the last accessToken if it
+  // is still valid.  If not default to interactive login behavior.
+  return this.storage.get('FacebookSocialProvider-last-access-token').then(
+      function(lastAccessToken) {
+    return this.isValidToken_(lastAccessToken).then(function(isValid) {
+      if (isValid) {
+        return lastAccessToken;
+      } else {
+        return this.getOAuthTokenInteractive_(loginOpts);
+      }
+    }.bind(this));
+  }.bind(this));
+};
+
+/*
+ * Launches an interactive Facebook OAuth login.
+ * Returns a Promise which fulfills with an OAuth token.
+ */
+FacebookSocialProvider.prototype.getOAuthTokenInteractive_ =
+    function(loginOpts) {
   var OAUTH_REDIRECT_URLS = [
-    "https://fmdppkkepalnkeommjadgbhiohihdhii.chromiumapp.org/",
     "https://www.uproxy.org/oauth-redirect-uri",
+    "https://fmdppkkepalnkeommjadgbhiohihdhii.chromiumapp.org/",
     "http://freedomjs.org/",
     'http://localhost:8080/'
   ];
@@ -31,11 +51,15 @@ FacebookSocialProvider.prototype.getOAuthToken_ = function() {
                 "&redirect_uri=" + encodeURIComponent(stateObj.redirect) +
                 "&state=" + encodeURIComponent(stateObj.state) +
                 "&response_type=token";
-    return oauth.launchAuthFlow(url, stateObj);
+    return oauth.launchAuthFlow(url, stateObj, loginOpts.interactive);
   }).then(function(responseUrl) {
-    return responseUrl.match(/access_token=([^&]+)/)[1];
-  }).catch(function (err) {
-    return Promise.reject('Login error: ' + err.message);
+    var accessToken = responseUrl.match(/access_token=([^&]+)/)[1];
+    if (loginOpts.rememberLogin) {
+      this.storage.set('FacebookSocialProvider-last-access-token', accessToken);
+    }
+    return accessToken;
+  }.bind(this)).catch(function (err) {
+    return Promise.reject('Login error: ' + err);
   });
 };
 
@@ -108,6 +132,30 @@ FacebookSocialProvider.prototype.facebookGet_ = function(endPoint) {
     // TODO: error checking
     xhr.onload = function() {
       fulfill(JSON.parse(this.response));
+    };
+    xhr.send();
+  });
+};
+
+/*
+ * Returns a Promise<boolean> that fulfills with true iff token is still valid.
+ */
+FacebookSocialProvider.prototype.isValidToken_ = function(token) {
+  return new Promise(function(fulfill, reject) {
+    if (!token) {
+      fulfill(false);
+      return;
+    }
+    var xhr = new XMLHttpRequest();
+    var url = 'https://graph.facebook.com/v2.1/me?access_token=' + token +
+        '&format=json&redirect=false';
+    xhr.open('GET', url);
+    xhr.onload = function() {
+      if (JSON.parse(this.response).error) {
+        fulfill(false);
+      } else {
+        fulfill(true);
+      }
     };
     xhr.send();
   });
--- src/google-social-provider.js
+++ src/google-social-provider.js
@@ -7,7 +7,24 @@ GoogleSocialProvider = function(dispatchEvent) {
 GoogleSocialProvider.prototype = new FirebaseSocialProvider();
 
 GoogleSocialProvider.prototype.authenticate_ = function(firebaseRef) {
-  return this.oauth_(firebaseRef);
+  return this.oauth_(firebaseRef).then(function(authData) {
+    console.log('authData', authData);
+    // TODO: displayName isn't always available.
+    // TODO: if both displayName and email aren't available we should
+    // probably reject?
+    this.name = authData.google.displayName || authData.google.email;
+    this.email = authData.google.email;
+
+    // TODO: using this.allUsersUrl_ from parent class is hacky...
+    var profileUrl = this.allUsersUrl_ + '/' + authData.uid + '/profile';
+    console.log('profileUrl ' + profileUrl);
+    var profileRef = new Firebase(profileUrl);
+    // Note this is the name that appears in the UserProfile, it may contain
+    // spaces, etc.
+    profileRef.update({name: this.name});
+
+    return authData;
+  }.bind(this));
 };
 
 /*
@@ -22,10 +39,9 @@ GoogleSocialProvider.prototype.getOAuthToken_ = function() {
   ];
   var OAUTH_CLIENT_ID = '746567772449-jkm5q5hjqtpq5m9htg9kn0os8qphra4d' +
       '.apps.googleusercontent.com';
-  var OAUTH_SCOPE = 'https://www.googleapis.com/auth/plus.login%20' +
-      'https://www.googleapis.com/auth/plus.me%20' +
+  var OAUTH_SCOPE = 'https://www.googleapis.com/auth/plus.me%20' +
       'https://www.googleapis.com/auth/userinfo.email%20' +
-      'https://www.googleapis.com/auth/userinfo.profile';
+      'https://www.googleapis.com/auth/gmail.send';
 
   var oauth = freedom["core.oauth"]();
   return oauth.initiateOAuth(OAUTH_REDIRECT_URLS).then(function(stateObj) {
@@ -51,20 +67,52 @@ GoogleSocialProvider.prototype.getOAuthToken_ = function() {
  * fetching) for each contact.
  */
 GoogleSocialProvider.prototype.loadContacts_ = function() {
-  // TODO: should we periodically check for new friends?  Or just force
-  // users to logout then login again to detect new friends?
-  this.googleGet_('plus/v1/people/me/people/visible').then(function(resp) {
-    for (var i = 0; i < resp.items.length; ++i) {
-      var friend = resp.items[i];
-      this.addUserProfile_({
-        userId: friend.id,
-        name: friend.displayName,
-        imageData: friend.image.url,
-        url: friend.url
-      });
-    }
-  }.bind(this)).catch(function(e) {
-    this.logger.error('Error loading Google users', e);
+  // TODO: this is almost identical to Email
+  // (just changed google: to google:)...
+  // should combine if we really go forward with this stuff.
+
+  var allFriendsRef = new Firebase(
+    this.allUsersUrl_ + '/google:' + this.getUserId_() + '/friends/');
+  // TODO: is on correct or should it be once?
+  this.on_(allFriendsRef, 'child_added', function(snapshot) {
+    var friendId = snapshot.key().substr('google:'.length);
+    console.log('got friendId ' + friendId);
+    var friendProfileRef = new Firebase(
+      this.allUsersUrl_ + '/google:' + friendId + '/profile/');
+    friendProfileRef.once('value', function(snapshot) {
+      if (this.loginState_.userProfiles[friendId]) {
+        // TODO: kinda hacky.. This ignores newly added profiles as a result of
+        // processing friendRequests
+        return;
+      }
+      console.log('adding pre-existing profile, ' + friendId + ', ' + snapshot.val().name);
+      this.addUserProfile_({userId: friendId, name: snapshot.val().name});
+    }.bind(this), function(error) {
+      // TODO: if the friend didn't actually add us (accept our friendRequest)
+      // this will fail to read due to permissions..  Not actually an error
+      console.log('error is !!!! ', error);
+    }.bind(this));
+  }.bind(this));
+
+  // Monitor friend requests.
+  // TODO: these should all be permissioned already, but should we double check?
+  var friendRequestsRef = new Firebase(
+    this.allUsersUrl_ + '/google:' + this.getUserId_() + '/friendRequestsWithToken/');
+  // TODO: is on correct or should it be once?
+  this.on_(friendRequestsRef, 'child_added', function(snapshot) {
+
+    console.log('got friendRequest ' + snapshot.key() + ', ', snapshot.val());
+    console.log('... ' + snapshot.child(snapshot.key()).val());
+    snapshot.forEach(function(childSnapshot) {
+      var val = childSnapshot.val();
+      var friendId = val.userId;
+      if (this.loginState_.userProfiles[friendId]) {
+        // Sanity check that friend doesn't already exist.
+        return;
+      }
+      this.addUserProfile_({userId: friendId, name: val.name});
+      // TODO: delete snapshot!
+    }.bind(this));
   }.bind(this));
 };
 
@@ -108,6 +156,111 @@ GoogleSocialProvider.prototype.googleGet_ = function(endPoint) {
     };
     xhr.send();
   });
+};
+
+/*
+ * Makes get request to Google endpoint, and returns a Promise which
+ * fulfills with the response object.
+ */
+GoogleSocialProvider.prototype.googlePost_ = function(endPoint, data) {
+  if (!this.loginState_) {
+    throw 'Not signed in';
+  }
+  var xhr = new XMLHttpRequest();
+  var url = 'https://www.googleapis.com/' + endPoint +
+      '?key=AIzaSyA1Q7SiEeUdSJwansl2AUFXLpVdnsXUzYg&alt=json';
+  xhr.open('POST', url);
+  xhr.setRequestHeader(
+      'Authorization',
+      'Bearer ' + this.loginState_.authData.google.accessToken);
+  xhr.setRequestHeader(
+      'Content-Type', 'application/json');
+  return new Promise(function(fulfill, reject) {
+    // TODO: error checking
+    xhr.onload = function() {
+      fulfill(JSON.parse(this.response));
+    };
+    xhr.send(data);
+  });
+};
+
+GoogleSocialProvider.prototype.sendInviteEmail = function(friendEmail, token) {
+  var email ='"Content-Type: text/plain; charset="us-ascii"\n' +
+      'MIME-Version: 1.0\n' +
+      'Content-Transfer-Encoding: 7bit\n' +
+      'to: ' + friendEmail + '\n' +
+      'from: ' + this.email + '\n' +  // TODO: need to get the users email!!!
+      'subject: Join me on uProxy\n\n' +
+      'Click here to join me on uProxy ' + token;
+  this.googlePost_('gmail/v1/users/me/messages/send',
+      JSON.stringify({raw: btoa(email)}));
+};
+
+// TODO: same as Email (just changed simplelogin: to google:)
+GoogleSocialProvider.prototype.sendIntroductionToken = function(friendEmail) {
+  if (!this.loginState_) {
+    throw 'Error in FirebaseSocialProvider.getUserId_: not logged in';
+  }
+  // TODO: clarify name, is it token, code, secret, etc?
+  var uid = this.loginState_.authData.uid;
+  var permissionToken = Math.floor(Math.random() * 100000000000);
+  var tokenUrl = this.allUsersUrl_ + '/' + uid + '/generatedInviteTokens/'+ permissionToken;
+  console.log('tokenUrl ' + tokenUrl);
+  var tokenRef = new Firebase(tokenUrl);
+  tokenRef.set({timestamp: Date.now()});
+
+
+  // TODO: this userId is the numeric ID, not the name!!!
+  // TODO: make userId and name consistent!!
+  var jsonString = JSON.stringify(
+    {userId: this.getUserId_(), token: permissionToken, name: this.name});
+  // TODO: this formatting duplciates work done in uProxy!!!
+  this.sendInviteEmail(friendEmail, 'https://www.uproxy.org/invite/Google+/' + btoa(jsonString));
+  return Promise.resolve(btoa(jsonString));
+};
+
+// TODO: same as Email (just changed simplelogin: to google:)
+GoogleSocialProvider.prototype.addContact = function(encodedToken) {
+  return new Promise(function(F, R) {
+    console.log('addContact called with ' + encodedToken);
+    // TODO: try/catch
+    var data = JSON.parse(atob(encodedToken));
+    console.log('data ', data);
+
+    // Try to write to friends friendRequest folder
+    var myUserId = this.getUserId_();
+    var myName = this.name;
+    var friendUserId = data.userId;
+    var friendName = data.name;
+    var token = data.token;
+
+    // Sanity check
+    if (friendUserId === myUserId) {
+      return R('friendId matches self'); // TODO: better error
+    }
+
+    var receivedInviteTokensRef = new Firebase(
+      this.allUsersUrl_ + '/google:' + myUserId + '/receivedInviteTokens/' + token);
+    receivedInviteTokensRef.set({received: true}, function(error) {
+      console.log('pushed');
+      if (error) {
+        console.error('error writing to receivedInviteTokens');  // should never happen
+        return R('error writing to receivedInviteTokens');
+      }
+
+      var friendRequestUrl = this.allUsersUrl_ + '/google:' + friendUserId + '/friendRequestsWithToken/' + token;
+      console.log('friendRequestUrl: ' + friendRequestUrl);
+      var friendRequestRef = new Firebase(friendRequestUrl);
+      friendRequestRef.push({userId: myUserId, name: myName}, function(error) {
+        console.log('push to friendRequestRef completed with error ' + error);
+        if (error) {
+          return R('Not permissioned to add friend');
+        }
+        F();
+        this.addUserProfile_({userId: friendUserId, name: friendName});
+      }.bind(this));
+    }.bind(this));
+  }.bind(this));
 };
 
 
