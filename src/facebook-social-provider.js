d4604266e6d875431a078ae842759a0342ba3fd4
Daniel Borkan <dborkan@dborkan-macbookpro2.roam.corp.google.com>
2015-08-19 12:51:00 -0400

    Refactor common code

--- src/facebook-social-provider.js
+++ src/facebook-social-provider.js
@@ -80,64 +80,6 @@ FacebookSocialProvider.prototype.getOAuthTokenInteractive_ =
 };
 
 /*
- * Loads contacts of the logged in user, and calls this.addUserProfile_
- * and this.updateUserProfile_ (if needed later, e.g. for async image
- * fetching) for each contact.
- */
-FacebookSocialProvider.prototype.loadContacts_ = function() {
-  // TODO: this is almost identical to Email
-  // (just changed facebook: to facebook:)...
-  // should combine if we really go forward with this stuff.
-
-  var allFriendsRef = new Firebase(
-    this.allUsersUrl_ + '/facebook:' + this.getUserId_() + '/friends/');
-  // TODO: is on correct or should it be once?
-  this.on_(allFriendsRef, 'child_added', function(snapshot) {
-    var friendId = snapshot.key().substr('facebook:'.length);
-    console.log('got friendId ' + friendId);
-    var friendProfileRef = new Firebase(
-      this.allUsersUrl_ + '/facebook:' + friendId + '/profile/');
-    friendProfileRef.once('value', function(snapshot) {
-      if (!snapshot.exists()) {
-        console.error('Profile not found for friend ' + friendId);
-        return;
-      } else if (this.loginState_.userProfiles[friendId]) {
-        // TODO: kinda hacky.. This ignores newly added profiles as a result of
-        // processing friendRequests
-        return;
-      }
-      console.log('adding pre-existing profile, ' + friendId + ', ' + snapshot.val().name);
-      this.addUserProfile_({userId: friendId, name: snapshot.val().name});
-    }.bind(this), function(error) {
-      // TODO: if the friend didn't actually add us (accept our friendRequest)
-      // this will fail to read due to permissions..  Not actually an error
-      console.log('error is !!!! ', error);
-    }.bind(this));
-  }.bind(this));
-
-  // Monitor friend requests.
-  // TODO: these should all be permissioned already, but should we double check?
-  var friendRequestsRef = new Firebase(
-    this.allUsersUrl_ + '/facebook:' + this.getUserId_() + '/friendRequestsWithToken/');
-  // TODO: is on correct or should it be once?
-  this.on_(friendRequestsRef, 'child_added', function(snapshot) {
-
-    console.log('got friendRequest ' + snapshot.key() + ', ', snapshot.val());
-    console.log('... ' + snapshot.child(snapshot.key()).val());
-    snapshot.forEach(function(childSnapshot) {
-      var val = childSnapshot.val();
-      var friendId = val.userId;
-      if (this.loginState_.userProfiles[friendId]) {
-        // Sanity check that friend doesn't already exist.
-        return;
-      }
-      this.addUserProfile_({userId: friendId, name: val.name});
-      // TODO: delete snapshot!
-    }.bind(this));
-  }.bind(this));
-};
-
-/*
  * Returns UserProfile object for the logged in user.
  */
 FacebookSocialProvider.prototype.getMyUserProfile_ = function() {
@@ -175,70 +117,6 @@ FacebookSocialProvider.prototype.facebookGet_ = function(endPoint) {
     };
     xhr.send();
   });
-};
-
-// TODO: same as Email (just changed simplelogin: to facebook:)
-FacebookSocialProvider.prototype.getInviteToken = function() {
-  if (!this.loginState_) {
-    throw 'Error in FirebaseSocialProvider.getUserId_: not logged in';
-  }
-  // TODO: clarify name, is it token, code, secret, etc?
-  var uid = this.loginState_.authData.uid;
-  var permissionToken = Math.floor(Math.random() * 100000000000);
-  var tokenUrl = this.allUsersUrl_ + '/' + uid + '/generatedInviteTokens/'+ permissionToken;
-  console.log('tokenUrl ' + tokenUrl);
-  var tokenRef = new Firebase(tokenUrl);
-  tokenRef.set({timestamp: Date.now()});
-
-  // TODO: this userId is the numeric ID, not the name!!!
-  // TODO: make userId and name consistent!!
-  var jsonString = JSON.stringify(
-    {userId: this.getUserId_(), token: permissionToken, name: this.name});
-  return Promise.resolve(btoa(jsonString));
-};
-
-// TODO: same as Email (just changed simplelogin: to facebook:)
-FacebookSocialProvider.prototype.addContact = function(encodedToken) {
-  return new Promise(function(F, R) {
-    console.log('addContact called with ' + encodedToken);
-    // TODO: try/catch
-    var data = JSON.parse(atob(encodedToken));
-    console.log('data ', data);
-
-    // Try to write to friends friendRequest folder
-    var myUserId = this.getUserId_();
-    var myName = this.name;
-    var friendUserId = data.userId;
-    var friendName = data.name;
-    var token = data.token;
-
-    // Sanity check
-    if (friendUserId === myUserId) {
-      return R('friendId matches self'); // TODO: better error
-    }
-
-    var receivedInviteTokensRef = new Firebase(
-      this.allUsersUrl_ + '/facebook:' + myUserId + '/receivedInviteTokens/' + token);
-    receivedInviteTokensRef.set({received: true}, function(error) {
-      console.log('pushed');
-      if (error) {
-        console.error('error writing to receivedInviteTokens');  // should never happen
-        return R('error writing to receivedInviteTokens');
-      }
-
-      var friendRequestUrl = this.allUsersUrl_ + '/facebook:' + friendUserId + '/friendRequestsWithToken/' + token;
-      console.log('friendRequestUrl: ' + friendRequestUrl);
-      var friendRequestRef = new Firebase(friendRequestUrl);
-      friendRequestRef.push({userId: myUserId, name: myName}, function(error) {
-        console.log('push to friendRequestRef completed with error ' + error);
-        if (error) {
-          return R('Not permissioned to add friend');
-        }
-        F();
-        this.addUserProfile_({userId: friendUserId, name: friendName});
-      }.bind(this));
-    }.bind(this));
-  }.bind(this));
 };
 
 FacebookSocialProvider.prototype.sendEmail = function(friendEmail, subject, body) {
